663aad4a44d7213ca06cfbf0b7931726d741fae8
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-25 02:16:51 +0700

    chore(golang-alpha-indo-soft): prepared elasticsearch pkg

---
+++ pkg/elasticsearch/elasticsearch.go
@@ -0,0 +1,26 @@
+package postgres
+
+import (
+	elastic "github.com/elastic/go-elasticsearch/v7"
+	"github.com/pkg/errors"
+)
+
+type Config struct {
+	Url string
+}
+
+func NewElasticSearchClient(cfg *Config) (*elastic.Client, error) {
+	client, err := elastic.NewClient(elastic.Config{
+		Addresses: []string{cfg.Url},
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "pkg.elasticsearch.NewClient")
+	}
+
+	_, err = client.Info()
+	if err != nil {
+		return nil, errors.Wrap(err, "pkg.elasticsearch.client.Info")
+	}
+
+	return client, nil
+}

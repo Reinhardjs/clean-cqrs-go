918bd8545dec468d3894cea90c71cef98888306a
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-25 03:08:13 +0700

    chore(golang-alpha-indo-soft): refactor filename of article models

--- internal/models/article.go
+++ internal/models/article.go
@@ -11,3 +11,16 @@ type Article struct {
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
+
+func (post *Article) Validate() (string, bool) {
+
+	if post.Title == "" {
+		return "Title should be on the payload", false
+	}
+
+	if post.Content == "" {
+		return "Content should be on the payload", false
+	}
+
+	return "Payload is valid", true
+}

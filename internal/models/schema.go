7709d3f03684b07f6b9afd33d9098bf92f2a0769
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-25 01:04:01 +0700

    chore(golang-alpha-indo-soft): added article model

---
+++ internal/models/schema.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"time"
+)
+
+type Article struct {
+	ID        string    `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}

3c155d94fdd42c433729300db5a69f2959c8d2f6
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-25 01:53:35 +0700

    chore(golang-alpha-indo-soft): refactored redis pkg

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gomodule/redigo/redis"
 	"github.com/pkg/errors"
 )
 
-func NewRedisConn() (redis.Conn, error) {
-	conn, err := redis.Dial("tcp", "localhost:6379")
+type Config struct {
+	Host string
+	Port string
+}
+
+func NewRedisConn(cfg *Config) (redis.Conn, error) {
+	redisUri := fmt.Sprintf("%s:%s", cfg.Host, cfg.Port)
+	conn, err := redis.Dial("tcp", redisUri)
 
 	if err != nil {
 		return nil, errors.Wrap(err, "redis.ConnectConfig")

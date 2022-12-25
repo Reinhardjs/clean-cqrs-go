549f0dbbfe89d48466225759880ce2c50eafd053
reinhardjs <reinhardjsilalahi@gmail.com>
2022-12-25 07:05:47 +0700

    chore(golang-alpha-indo-soft): added constants

---
+++ pkg/constants/constants.go
@@ -0,0 +1,15 @@
+package constants
+
+const (
+	PostgresHost          = "POSTGRES_HOST"
+	PostgresDBName        = "POSTGRES_DB"
+	PostgresUser          = "POSTGRES_USER"
+	PostgresPassword      = "POSTGRES_PASSWORD"
+	PostgresSSLMode       = "POSTGRES_SSLMODE"
+	RedisHost             = "REDIS_HOST"
+	RedisPort             = "REDIS_PORT"
+	NatsAddress           = "NATS_ADDRESS"
+	ElasticsearchAddress  = "ELASTICSEARCH_ADDRESS"
+	ElasticsearchUsername = "ELASTICSEARCH_USERNAME"
+	ElasticsearchPassword = "ELASTICSEARCH_USERNAME"
+)
